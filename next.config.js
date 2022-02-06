f8de8ea36d77dbf61deeb42827046bc98654a345
Yvonne Hoang <vonne.hoang@gmail.com>
2022-02-05 18:57:10 -0800

    Add redirects and domains

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["msavatar1.nexon.net", "i.imgur.com"]
+  },
+  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 }
